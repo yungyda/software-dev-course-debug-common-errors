d4b9910689c964f2be9f8afe5133e6c371744d1c
yungyda <justinbailey821@gmail.com>
2025-03-18 14:57:05 -0500

    Fixed syntax, runtime, and logical errors in programs A, B, and C

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -24,7 +24,7 @@ Think about which debugging methods you found most useful and how you might appl
 // Description:
 // This program is intended to display a simple prompt in the console but fails to run.
 
-console.log("Welcome to the bootcamp
+console.log("Welcome to the bootcamp");
 
 // What’s Wrong?
 
@@ -33,7 +33,7 @@ console.log("Welcome to the bootcamp
 // Description:
 // This code attempts to multiply each number in an array by 2 and display the results. However, it crashes at runtime.
 
-let numbers = [2, 4, "eight"];
+let numbers = [2, 4,8];// Changed "eight" to 8
 for (let i = 0; i < numbers.length; i++) {
   let doubled = numbers[i] * 2;
   console.log(doubled);
@@ -51,12 +51,12 @@ function isPrime(num) {
   if (num < 2) return false;
   for (let i = 2; i < num; i++) {
     if (num % i === 0) {
-      return true;  // Supposed to indicate num is NOT prime
+      return false;  // Supposed to indicate num is NOT prime
     }
   }
-  return false; // Supposed to indicate num IS prime
+  return true; // Supposed to indicate num IS prime
 }
 
-console.log(isPrime(7)); // Expected true but gets false
+console.log(isPrime(7)); // Expected true 
 
 // What’s Wrong?
